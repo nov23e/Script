ad8235b3802b7bef623287d23593e54579f296e1
Cyril Wong <kli@live.com>
2019-03-31 16:29:39 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -4,7 +4,7 @@
 #   System Required:  CentOS 6+, Debian7+, Ubuntu12+              #
 #   Description: Auto install latest kernel for TCP BBR           #
 #   Author: Cyril <i@lia.im>                                      #
-#   Intro:  https://www.mlio.net/                                 #
+#   Intro:  https://www.mlio.net/bbr.html                         #
 #=================================================================#
 #
 
@@ -378,7 +378,7 @@ echo " Kernel  : $kern"
 echo "----------------------------------------"
 echo " Auto install latest kernel for TCP BBR"
 echo
-echo " URL: https://www.mlio.net/"
+echo " URL: https://www.mlio.net/bbr.html"
 echo "----------------------------------------"
 echo
 echo "Press any key to start...or Press Ctrl+C to cancel"
